aa76052d0d07d2e369315a060d636af783fa9df5
Max Gonzih <gonzih@gmail.com>
2021-03-18 09:36:51 -0400

    Change chart repsonse

--- src/main/scala/chart/Chart.scala
+++ src/main/scala/chart/Chart.scala
@@ -6,63 +6,18 @@ import app.paperhands.format.Format
 case class Spot(x: Int, y: Int)
 
 case class ChartResponse(
-    spots: List[Spot],
-    min_y: Int,
-    min_x: Int,
-    max_y: Int,
-    max_x: Int,
-    y_titles: Map[Int, String],
-    x_titles: Map[Int, String]
+    data: List[Int],
+    titles: List[String]
 )
 
 object Chart {
-  def includeLabel(i: Int, limit: Int, max: Int): Boolean =
-    0.to(max - 1).by(max / limit).contains(i)
-
-  def getYLabels(min: Int, limit: Int, max: Int) = {
-    val total = max - min
-    val step = total / limit
-
-    min
-      .to(max)
-      .by(step)
-      .map(i => {
-        val v = i + min
-        v -> v.toString
-      })
-      .toMap
-  }
+  private def format(v: model.TimeSeries) =
+    Format.HHmm(v.time)
 
   def fromTimeSeries(input: List[model.TimeSeries]): ChartResponse = {
-    val maxX = input.length
-    val minX = 0
+    val data = input.map(_.value)
+    val titles = input.map(format)
 
-    val maxY = input.map(_.value).maxOption.getOrElse(1000)
-    val minY = input.map(_.value).minOption.getOrElse(0)
-
-    val yTitles = getYLabels(minY, 5, maxY)
-
-    val spots = input.zipWithIndex.map { case (v, index) =>
-      Spot(index, v.value)
-    }
-
-    val xTitles = input.zipWithIndex
-      .filter { case (v, index) =>
-        includeLabel(index, 5, input.length)
-      }
-      .map { case (v, index) =>
-        index -> Format.HHmm(v.time)
-      }
-      .toMap
-
-    ChartResponse(
-      spots,
-      minY,
-      minX,
-      maxY,
-      maxX,
-      yTitles,
-      xTitles
-    )
+    ChartResponse(data, titles)
   }
 }
--- src/test/scala/ChartTest.scala
+++ src/test/scala/ChartTest.scala
@@ -2,27 +2,23 @@ import minitest._
 import app.paperhands.chart.Chart
 
 object ChartTestSuite extends SimpleTestSuite {
-  test("includeLabel fn 20 items") {
-    val labels = 0
-      .to(20)
-      .filter(Chart.includeLabel(_, 5, 20))
-
-    assertEquals(labels, Vector(0, 4, 8, 12, 16))
+  test("dummy") {
+    assert(true)
   }
 
-  test("includeLabel fn 120 items") {
-    val labels = 0
-      .to(120)
-      .filter(Chart.includeLabel(_, 5, 120))
+  // test("includeLabel fn 120 items") {
+  //   val labels = 0
+  //     .to(120)
+  //     .filter(Chart.includeLabel(_, 5, 120))
 
-    assertEquals(labels, Vector(0, 24, 48, 72, 96))
-  }
+  //   assertEquals(labels, Vector(0, 24, 48, 72, 96))
+  // }
 
-  test("includeLabel fn 900 items") {
-    val labels = 0
-      .to(900)
-      .filter(Chart.includeLabel(_, 5, 900))
+  // test("includeLabel fn 900 items") {
+  //   val labels = 0
+  //     .to(900)
+  //     .filter(Chart.includeLabel(_, 5, 900))
 
-    assertEquals(labels, Vector(0, 180, 360, 540, 720))
-  }
+  //   assertEquals(labels, Vector(0, 180, 360, 540, 720))
+  // }
 }
