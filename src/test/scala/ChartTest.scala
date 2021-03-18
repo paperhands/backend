d8471290af414f4a6450732a7add21b6008d5b1e
Max Gonzih <gonzih@gmail.com>
2021-03-18 13:50:12 -0400

    rm

--- src/test/scala/ChartTest.scala
+++ src/test/scala/ChartTest.scala
@@ -5,20 +5,4 @@ object ChartTestSuite extends SimpleTestSuite {
   test("dummy") {
     assert(true)
   }
-
-  // test("includeLabel fn 120 items") {
-  //   val labels = 0
-  //     .to(120)
-  //     .filter(Chart.includeLabel(_, 5, 120))
-
-  //   assertEquals(labels, Vector(0, 24, 48, 72, 96))
-  // }
-
-  // test("includeLabel fn 900 items") {
-  //   val labels = 0
-  //     .to(900)
-  //     .filter(Chart.includeLabel(_, 5, 900))
-
-  //   assertEquals(labels, Vector(0, 180, 360, 540, 720))
-  // }
 }
