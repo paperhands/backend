56164a023114d1ece71a1529320e52bb3a1af0e2
Boyan Bonev <boyan@typed.wtf>
2021-03-20 19:38:13 +0200

    Add defaults to the timestamp migration.

--- src/main/resources/migrations/V006__timestamp_without_tz.sql
+++ src/main/resources/migrations/V006__timestamp_without_tz.sql
@@ -1,3 +1,6 @@
-ALTER TABLE engagements ALTER created_time TYPE timestamp USING created_time AT TIME ZONE 'UTC';
-ALTER TABLE sentiments ALTER created_time TYPE timestamp USING created_time AT TIME ZONE 'UTC';
-ALTER TABLE ocr_cache ALTER created_time TYPE timestamp USING created_time AT TIME ZONE 'UTC';
+ALTER TABLE engagements ALTER created_time TYPE timestamp without time zone USING created_time AT TIME ZONE 'UTC';
+ALTER TABLE engagements ALTER created_time SET DEFAULT (now() at time zone 'utc') ;
+ALTER TABLE sentiments ALTER created_time TYPE timestamp without time zone USING created_time AT TIME ZONE 'UTC';
+ALTER TABLE sentiments ALTER created_time SET DEFAULT (now() at time zone 'utc');
+ALTER TABLE ocr_cache ALTER created_time TYPE timestamp without time zone USING created_time AT TIME ZONE 'UTC';
+ALTER TABLE sentiments ALTER created_time SET DEFAULT (now() at time zone 'utc');
